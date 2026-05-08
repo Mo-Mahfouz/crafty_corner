a9acac38901275fc3f320847df25ef145aaf7dc5
Mo-Mahfouz <benmahfouz767@gmail.com>
2026-05-08 16:45:55 +0300

    category items (all_products css added)

--- resources/views/layouts/collections.blade.php
+++ resources/views/layouts/collections.blade.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
     <title>Nourhan Store | Lace Heirloom Gowns</title>
-    <link rel="stylesheet" href="Category items.css" />
+    <link rel="stylesheet" href="{{ asset('css/categoryItems.css') }}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+
 
 <body>
     <!-- HEADER -->
